3a70ad71d79b0b88cac2875b6a4187181eddf10b
nballar <69126984+nballar@users.noreply.github.com>
2022-02-08 18:06:05 +0000

    updated answer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,18 @@ import io.javalin.Javalin;
 public class Main {
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(4100);
+      Javalin app = Javalin.create().start(4100);
         
-        app.get("/", ctx -> ctx.result("Hello World!"));
+      app.get("/", ctx -> ctx.result("Hello World!"));
 
-        app.get("/welcome", ctx -> {
-            String name = ctx.queryParam("name");
-            ctx.result("Welcome " + name);
-        });
+      app.get("/welcome", ctx -> {
+        String name = ctx.queryParam("name");
+        ctx.result("Welcome " + name);
+      });
 
+      app.get("/favoriteBook", ctx -> {
+        String title = ctx.queryParam("title");
+        ctx.result("My favorite book is " + title);
+      });
     }
 }
